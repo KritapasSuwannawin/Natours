98be9188c5fcdd2af9c6de32e63fda999029058e
KritapasSuwannawin <kritapas.ms@gmail.com>
2023-02-17 12:27:00 -0500

    implement update and delete user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,10 @@ router.route('/login').post(authController.login);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 
+router.route('/updateMyPassword').patch(authController.protect, authController.updatePassword);
+router.route('/updateMe').patch(authController.protect, userController.updateMe);
+router.route('/deleteMe').delete(authController.protect, userController.deleteMe);
+
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
