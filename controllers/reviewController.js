5977340a4eeb118495b6ec4744ef88fe3ba13274
KritapasSuwannawin <kritapas.ms@gmail.com>
2023-02-19 16:08:28 -0500

    refactor controller into factory handler

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,28 +1,10 @@
 const Review = require('./../models/reviewModel');
-const catchAsync = require('../utils/catchAsync');
-const APIFeatures = require('../utils/apiFeatures');
+// const catchAsync = require('../utils/catchAsync');
+// const APIFeatures = require('../utils/apiFeatures');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) {
-    filter = { tour: req.params.tourId };
-  }
+const factory = require('./handlerFactory');
 
-  const features = new APIFeatures(Review.find(filter), req.query).filter().sort().limitFields().paginate();
-  // const doc = await features.query.explain();
-  const reviews = await features.dbQuery;
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      data: reviews,
-    },
-  });
-});
-
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourAndUserId = (req, res, next) => {
   if (!req.body.tour) {
     req.body.tour = req.params.tourId;
   }
@@ -31,12 +13,15 @@ exports.createReview = catchAsync(async (req, res, next) => {
     req.body.user = req.user.id;
   }
 
-  const newReview = await Review.create(req.body);
+  next();
+};
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      data: newReview,
-    },
-  });
-});
+exports.getAllReviews = factory.getAll(Review);
+
+exports.getReview = factory.getOne(Review);
+
+exports.createReview = factory.createOne(Review);
+
+exports.updateReview = factory.updateOne(Review);
+
+exports.deleteReview = factory.deleteOne(Review);
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -7,6 +7,11 @@ const router = express.Router({ mergeParams: true });
 
 router.use(authController.protect);
 
-router.route('/').get(reviewController.getAllReviews).post(authController.restrictTo('user'), reviewController.createReview);
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(authController.restrictTo('user'), reviewController.setTourAndUserId, reviewController.createReview);
+
+router.route('/:id').get(reviewController.getReview).patch(reviewController.updateReview).delete(reviewController.deleteReview);
 
 module.exports = router;
